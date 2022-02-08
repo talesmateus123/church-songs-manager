85601fd392a971e192fd5772db8e5acdc95e6c1d
Tales <talesmateus1999@hotmail.com>
2022-02-08 15:19:10 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'church-songs-manager',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
